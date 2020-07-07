51802999a410aa915bc81c0c9776bebeb0a7fbbc
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-07 19:48:19 +0600

    Update controller.py

---
+++ src/controller.py
@@ -0,0 +1,25 @@
+import re
+import logging
+from src.parser import IGParser
+from src.models import User
+
+
+def check_username(username: str) -> bool:
+    if len(username) >= 30:
+        logging.error('Invalid username: can contain only letters, numbers, periods and underscores')
+        return False
+    regex = '[^A-Za-z0-9_.]'
+    regex_res = re.compile(pattern=regex)
+    invalid = regex_res.search(username)
+    if invalid:
+        logging.error('Invalid username: length of username must be less than 30')
+        return False
+    return True
+
+
+def get_user_info(username: str) -> User:
+    if check_username(username=username):
+        parser = IGParser()
+        user = parser.get_user(username=username)
+        return user
+
