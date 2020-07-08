cc01f443f6763eb3b725d743635ed1b831992714
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-08 11:55:35 +0600

    add ApplicationError class from err_utils.py

--- src/controller.py
+++ src/controller.py
@@ -1,25 +1,32 @@
 import re
-import logging
+from typing import Union
+from src.err_utils import ApplicationError
 from src.parser import IGParser
 from src.models import User
 
 
-def check_username(username: str) -> bool:
-    if len(username) >= 30:
-        logging.error('Invalid username: can contain only letters, numbers, periods and underscores')
-        return False
+def check_username(username: str) -> Union[bool, ApplicationError]:
+    if len(username) > 30:
+        return ApplicationError(
+            message='Invalid username: length of username must be less than 30',
+            status_code=400
+        )
     regex = '[^A-Za-z0-9_.]'
-    re_res = re.compile(pattern=regex)
-    invalid = re_res.search(username)
+    regex_res = re.compile(pattern=regex)
+    invalid = regex_res.search(username)
     if invalid:
-        logging.error('Invalid username: length of username must be less than 30')
-        return False
+        return ApplicationError(
+            message='Invalid username: username can contain only letters, numbers, periods and underscores',
+            status_code=400
+        )
     return True
 
 
-def get_user_info(username: str) -> User:
-    if check_username(username=username):
+def get_user_info(username: str) -> Union[User, ApplicationError]:
+    validation = check_username(username=username)
+    if isinstance(validation, ApplicationError):
+        return validation
+    if validation:
         parser = IGParser()
         user = parser.get_user(username=username)
         return user
-
