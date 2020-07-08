9906324e5cecef19f39e838e0d23a0761fac85d5
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-08 14:24:16 +0600

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI, Query
 from src.controller import *
-from src.err_utils import ApplicationError
 
 app = FastAPI()
 
 
 @app.get('/profile')
 def get(username: str = Query(..., description='Collect data of IG profile')):
-    user = get_user_info(username=username)
-    if isinstance(user, ApplicationError):
-        return {'error':user}
-    return {'info': user}
+    return {'info': get_user_info(username=username)}
