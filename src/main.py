1062b8245111fd06b56258b53eaca6dbf1118f7c
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-10 15:00:24 +0600

    add try-except block

--- src/main.py
+++ src/main.py
@@ -14,23 +14,24 @@ app = FastAPI()
 )
 def get(username: str = Query(..., min_length=1, max_length=30, regex='^[a-z0-9_.]{1,30}$')):
     parser = IGParser()
-    _user = parser.get_user(username=username)
-    err, status_code = None, 200
-
-    if isinstance(_user, ApplicationError):
-        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        err = _user
-        _user = None
-
-    return JSONResponse(
-        status_code=status_code,
-        content=jsonable_encoder(
-            {
-                'data': _user,
-                'error': err
-            }
+    try:
+        _user = parser.get_user(username=username)
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder({'data': _user})
         )
-    )
+    except ApplicationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=jsonable_encoder(
+                {
+                    'error': {
+                        'code': 'APIError',
+                        'message': f'Failed to parse page for username: {e.message}'
+                    }
+                }
+            )
+        )
 
 
 @app.exception_handler(RequestValidationError)
@@ -39,7 +40,6 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder(
             {
-                'data': None,
                 'error': {
                     'code': 'BadRequest',
                     'message': 'Invalid username: username can contain only letters, numbers, '
