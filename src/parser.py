394a3435959ef074c9ed5e97f039b62302bb4d02
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-10 12:14:32 +0600

    add RegexError

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,9 @@
 import re
 import json
 import requests
-from http import HTTPStatus
 from typing import Union
 from src.models import User
-from src.err_utils import ApplicationError
+from src.err_utils import *
 
 
 class IGParser:
@@ -12,15 +11,14 @@ class IGParser:
     def __init__(self):
         self.ig_home = 'https://www.instagram.com/'
 
-    def get_user(self, username: str) -> Union[User, ApplicationError]:
+    def get_user(self, username: str) -> Union[User, ApplicationError, None]:
         r = requests.get(
             url=f'{self.ig_home}{username}',
             timeout=10
         )
         if r.ok:
             regex = r'(?<="graphql":{"user":).*(?=},"toast_)'
-            match = re.search(pattern=regex, string=r.text)
-            if match:
+            try:
                 regex_res = re.findall(pattern=regex, string=r.text)[0]
                 _user = json.loads(regex_res)
                 return User(
@@ -28,24 +26,14 @@ class IGParser:
                     url=f'{self.ig_home}{username}',
                     bio=_user.get('biography'),
                     username=username,
-                    fullname=_user['full_name'],
+                    fullname=_user.get('full_name'),
                     followers=_user.get('edge_followed_by', {}).get('count'),
                     followings=_user.get('edge_follow', {}).get('count'),
                     is_business=_user.get('is_business_account'),
                     is_private=_user.get('is_private'),
                     profile_picture_url=_user.get('profile_pic_url_hd')
                 )
-            else:
-                return ApplicationError(
-                    message=f'RegEx Error: Invalid Regex',
-                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR
-                )
+            except Exception:
+                return RegexError()
         else:
-            return ApplicationError(
-                message=f'Failed to get page for {username} ',
-                status_code=r.status_code
-            )
-
-if __name__=='__main__':
-    p = IGParser()
-    p.get_user('a.knsm')
+            return None
