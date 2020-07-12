fab4e59cdfddbae75d2530e746be1e253640d762
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-12 09:16:05 +0600

    add cache

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from src.parser import IGParser
 from src.err_utils import *
-
+from src.cache import Cache
 app = FastAPI()
 
 
@@ -13,24 +13,31 @@ app = FastAPI()
     description='Get profile info'
 )
 def get(username: str = Query(..., min_length=1, max_length=30, regex='^[a-z0-9_.]{1,30}$')):
-    parser = IGParser()
-    try:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=jsonable_encoder({'data':  parser.get_user(username=username)})
-        )
-    except ApplicationError as e:
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content=jsonable_encoder(
-                {
-                    'error': {
-                        'code': 'APIError',
-                        'message': f'Failed to parse page for username: {e.message}'
-                    }
-                }
+    cache = Cache()
+    cached = cache.get(username)
+    if cached:
+        return cached
+    else:
+        parser = IGParser()
+        try:
+            _user = parser.get_user(username=username)
+            cache.save_cache(_user)
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=jsonable_encoder({'data': _user})
             )
-        )
+        except ApplicationError as e:
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content=jsonable_encoder(
+                    {
+                        'error': {
+                            'code': 'APIError',
+                            'message': f'Failed to parse page for username: {e.message}'
+                        }
+                    }
+                )
+            )
 
 
 @app.exception_handler(RequestValidationError)
