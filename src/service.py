773cc4c389f953e456953b4a6214e72eaef1ddd7
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-19 20:16:12 +0600

    Create service.py

---
+++ src/service.py
@@ -0,0 +1,38 @@
+import redis
+import json
+from src.parser import IGParser
+from src.db import *
+from src.err_utils import ApplicationError
+from src.models import User
+from src.conf import RedisConfig
+
+
+def get_cache(username: str):
+    print(f"Returning the cached result for username {username}")
+    my_redis = redis.Redis(**RedisConfig)
+    cached = my_redis.get(username)
+    if cached is not None:
+        return User(**json.loads(cached))
+
+
+def save_cache(_user: User):
+    my_redis = redis.Redis(**RedisConfig)
+    print(f"Caching the result for username {_user.username}")
+    my_redis.setex(
+        name=_user.username,
+        time=3600,
+        value=_user.json()
+    )
+
+
+def get_user(username: str):
+    try:
+        parser = IGParser()
+        _user: User = parser.get_user(username=username)
+        if _user:
+            save_cache(_user)
+            insert_user_db(_user)
+        print(f"Returning the IGParser's result for username {username}")
+        return _user
+    except ApplicationError as e:
+        raise e
