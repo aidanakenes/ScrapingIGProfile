cf8b41957e962aeef8f057a561b03e0d889adbe3
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-23 16:02:58 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -45,4 +45,4 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8080, workers=4, log_level=None)
+    uvicorn.run('main:app', host='0.0.0.0', port=8080, workers=4, log_config=None)
