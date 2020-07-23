670a31f7d84aa659a134f52927227cd644909d0d
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-23 14:24:25 +0600

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,30 +1,34 @@
 import uvicorn
+
 from fastapi import FastAPI, Request, status, Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-from src.service import *
+
+from src.service.cache import Cache
+from src.service.parser import IGParser
+from src.models.models import User
+from src.utils.err_utils import ApplicationError, ValidationError
+from src.db.db import DB
 
 app = FastAPI()
 
 
 @app.get('/profile', description='Get profile info')
 def get(username: str = Query(..., min_length=1, max_length=30, regex='^[a-z0-9_.]{1,30}$')):
-    _user = get_cache(username=username)
+    my_redis = Cache()
+    _user = my_redis.get_cache(username=username)
     if _user is None:
         try:
-            _user = get_user(username=username)
+            parser = IGParser()
+            _user: User = parser.get_user(username=username)
+            my_redis.save_cache(user=_user)
+            my_db = DB()
+            my_db.insert_user(user=_user)
         except ApplicationError as e:
             return JSONResponse(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content=jsonable_encoder(
-                    {
-                        'error': {
-                            'code': 'APIError',
-                            'message': f'Failed to parse page for username: {e.message}'
-                        }
-                    }
-                )
+                content=jsonable_encoder({'error': e.__dict__})
             )
     return JSONResponse(
         content=jsonable_encoder({'data': _user})
@@ -35,17 +39,9 @@ def get(username: str = Query(..., min_length=1, max_length=30, regex='^[a-z0-9_
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder(
-            {
-                'error': {
-                    'code': 'BadRequest',
-                    'message': 'Invalid username: username can contain only letters, numbers, '
-                               'periods, underscores and be less than 30'
-                }
-            }
-        )
+        content=jsonable_encoder({'error': ValidationError.__dict__})
     )
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8080, workers=4)
+    uvicorn.run('main:app', host='0.0.0.0', port=8080, workers=4, log_level=None)
