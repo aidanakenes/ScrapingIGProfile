79839c89f9acc6bc9f078fdb0b61e019656c2186
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-25 19:21:42 +0600

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,11 +1,12 @@
 import logging
 
 
-def get_logger(name: str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(levelname)s: %(name)s %(message)s %(asctime)s',
-        datefmt='%Y-%m-%dT%H:%M'
-    )
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s: %(name)s %(message)s %(asctime)s',
+    datefmt='%Y-%m-%dT%H:%M'
+)
 
+
+def get_logger(name: str):
     return logging.getLogger(name)
