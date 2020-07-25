0d2f0d997dd3e1e478f77f60191b86c89688339e
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-07-25 19:18:34 +0600

    Update db.py

--- src/db/db.py
+++ src/db/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-from src.service.conf import MongoConfig
-from src.models.models import User
+from src.utils.conf import MongoConfig
+from src.models.user import User
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -13,6 +13,6 @@ class DB:
         self._my_db = client['my_db']
         self._users_collection = self._my_db['ig_users']
 
-    def insert_user(self, user: User):
+    async def insert_user(self, user: User):
         logger.info(f"Saving the result into database for username {user.username}")
         self._users_collection.update({'id': user.id}, user.dict(), upsert=True)
