3111cd955f9a74413ec279fcc2cdfc535eb9be4b
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-08-02 15:51:59 +0600

    ✂️ Refactor: fixed imports & added TimeoutError

--- src/service/parser.py
+++ src/service/parser.py
@@ -1,7 +1,7 @@
 import re
 import json
-import requests
 
+import requests
 from typing import Optional
 
 from src.models.models import User
@@ -17,29 +17,33 @@ class IGParser:
         self.ig_home = 'https://www.instagram.com/'
 
     def get_user(self, username: str) -> Optional[User]:
-        r = requests.get(
-            url=f'{self.ig_home}{username}',
-            timeout=10
-        )
-        if r.ok:
-            regex = r'(?<="graphql":{"user":).*(?=},"toast_)'
-            try:
-                regex_res = re.findall(pattern=regex, string=r.text)[0]
-                _user = json.loads(regex_res)
-                user = User(
-                    id=_user.get('id'),
-                    url=f'{self.ig_home}{username}',
-                    bio=_user.get('biography'),
-                    username=username,
-                    fullname=_user.get('full_name'),
-                    followers=_user.get('edge_followed_by', {}).get('count'),
-                    followings=_user.get('edge_follow', {}).get('count'),
-                    is_business=_user.get('is_business_account'),
-                    is_private=_user.get('is_private'),
-                    profile_picture_url=_user.get('profile_pic_url_hd')
-                )
-                logger.info(f"Returning the IGParser's result for username {username}")
-                return user
-            except Exception as e:
-                logger.error(f'Cannot parse page for {username}: {type(e)}')
-                raise RegexError()
+        try:
+            r = requests.get(
+                url=f'{self.ig_home}{username}',
+                timeout=10
+            )
+            if r.ok:
+                regex = r'(?<="graphql":{"user":).*(?=},"toast_)'
+                try:
+                    regex_res = re.findall(pattern=regex, string=r.text)[0]
+                    _user = json.loads(regex_res)
+                    user = User(
+                        id=_user.get('id'),
+                        url=f'{self.ig_home}{username}',
+                        bio=_user.get('biography'),
+                        username=username,
+                        fullname=_user.get('full_name'),
+                        followers=_user.get('edge_followed_by', {}).get('count'),
+                        followings=_user.get('edge_follow', {}).get('count'),
+                        is_business=_user.get('is_business_account'),
+                        is_private=_user.get('is_private'),
+                        profile_picture_url=_user.get('profile_pic_url_hd')
+                    )
+                    logger.info(f"Returning the IGParser's result for username {username}")
+                    return user
+        except TimeoutError as e:
+            logger.error(f'Cannot parse page for {username}: {type(e)}')
+            raise ApplicationError()
+        except Exception as e:
+            logger.error(f'Cannot parse page for {username}: {type(e)}')
+            raise RegexError()
